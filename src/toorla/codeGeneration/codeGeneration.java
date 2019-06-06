a8dc9e7e1c8aea50bc426f34a1cb0240960d4b84
mhadih <m.hadi.hojjat@gmail.com>
2019-06-06 15:52:02 +0430

    create method Declaration

--- src/toorla/codeGeneration/codeGeneration.java
+++ src/toorla/codeGeneration/codeGeneration.java
@@ -8,13 +8,17 @@ import toorla.ast.declaration.classDecs.classMembersDecs.ClassMemberDeclaration;
 import toorla.ast.declaration.classDecs.classMembersDecs.FieldDeclaration;
 import toorla.ast.declaration.classDecs.classMembersDecs.MethodDeclaration;
 import toorla.ast.declaration.localVarDecs.ParameterDeclaration;
-import toorla.ast.expression.MethodCall;
-import toorla.ast.statement.Block;
-import toorla.ast.statement.Conditional;
-import toorla.ast.statement.Statement;
-import toorla.ast.statement.While;
+import toorla.ast.expression.*;
+import toorla.ast.expression.binaryExpression.*;
+import toorla.ast.expression.unaryExpression.Neg;
+import toorla.ast.expression.unaryExpression.Not;
+import toorla.ast.expression.value.BoolValue;
+import toorla.ast.expression.value.IntValue;
+import toorla.ast.expression.value.StringValue;
+import toorla.ast.statement.*;
 import toorla.ast.statement.localVarStats.LocalVarDef;
 import toorla.ast.statement.localVarStats.LocalVarsDefinitions;
+import toorla.ast.statement.returnStatement.Return;
 import toorla.compileErrorException.nameErrors.*;
 import toorla.symbolTable.SymbolTable;
 import toorla.symbolTable.exceptions.ItemAlreadyExistsException;
@@ -68,15 +72,6 @@ public class codeGeneration extends Visitor<Void> {
 
     @Override
     public Void visit(LocalVarDef localVarDef) {
-        try {
-            SymbolTable.top()
-                    .put(new LocalVariableSymbolTableItem(localVarDef.getLocalVarName().getName(), newLocalVarIndex));
-        } catch (ItemAlreadyExistsException e) {
-            LocalVarRedefinitionException ee = new LocalVarRedefinitionException(
-                    localVarDef.getLocalVarName().getName(), localVarDef.line, localVarDef.col);
-            localVarDef.addError(ee);
-        }
-        newLocalVarIndex++;
         return null;
     }
 
@@ -84,14 +79,14 @@ public class codeGeneration extends Visitor<Void> {
     public Void visit(ClassDeclaration classDeclaration) {
         SymbolTable.pushFromQueue();
         try {
-            File file = new File("../artifact" + classDeclaration.getName().getName() +".j");/////////need work
+            File file = new File("../artifact" + classDeclaration.getName().getName() +".j");
             boolean fvar = file.createNewFile();
-             if (fvar){
-                  System.out.println("File has been created successfully");
-             }
-             else{
-                  System.out.println("File already present at the specified location");
-             }
+            if (fvar){
+                System.out.println("File has been created successfully");
+            }
+            else{
+                System.out.println("File already present at the specified location");
+            }
         } catch (IOException e) {
             System.out.println("Exception Occurred:");
             e.printStackTrace();
@@ -101,7 +96,7 @@ public class codeGeneration extends Visitor<Void> {
             fw.write(".class public" + classDeclaration.getName().getName());
             String father;
             if(classDeclaration.getParentName() == null) {
-                father = "java/lang/Object";
+                father = "Any";
             }
             else
                 father = classDeclaration.getParentName().getName();
@@ -161,45 +156,9 @@ public class codeGeneration extends Visitor<Void> {
 
     @Override
     public Void visit(ParameterDeclaration parameterDeclaration) {
-        try {
-            LocalVariableSymbolTableItem paramItem = new LocalVariableSymbolTableItem(parameterDeclaration.getIdentifier().getName(), newLocalVarIndex);
-            paramItem.setVarType(parameterDeclaration.getType());
-            SymbolTable.top().put(
-                    paramItem
-            );
-
-        } catch (ItemAlreadyExistsException e) {
-            LocalVarRedefinitionException ee = new LocalVarRedefinitionException(
-                    parameterDeclaration.getIdentifier().getName(), parameterDeclaration.line,
-                    parameterDeclaration.col);
-            parameterDeclaration.addError(ee);
-        }
-        newLocalVarIndex++;
         return null;
     }
 
-    @Override
-    public Void visit(MethodDeclaration methodDeclaration) {
-        SymbolTable.pushFromQueue();
-        try{
-            String access,paramType= "",returnType;
-            if (methodDeclaration.getAccessModifier() == AccessModifier.ACCESS_MODIFIER_PRIVATE )
-                access = "praivate";
-            else
-                access = "public";
-            for (ParameterDeclaration arg : methodDeclaration.getArgs())
-                paramType += convertFieldType(arg.getType());
-            returnType = convertFieldType(methodDeclaration.getReturnType());
-            fw.write(".method " + access + " " + methodDeclaration.getName().getName() + "(" + paramType +")" + returnType );
-            fw.write(".limit stack 1000");
-            fw.write(".limit locals 1000");
-            for (Statement stmt : methodDeclaration.getBody())
-                stmt.accept(this);
-            fw.write(".end method");
-        SymbolTable.pop();
-        }catch (IOException e){}
-        return null;
-    }
 
     @Override
     public Void visit(LocalVarsDefinitions localVarsDefinitions) {
@@ -248,5 +207,173 @@ public class codeGeneration extends Visitor<Void> {
         SymbolTable.pop();
         return null;
     }
+
+    public Void visit(Plus plusExpr) {
+
+        return null;
+    }
+
+    public Void visit(Minus minusExpr) {
+
+        return null;
+    }
+
+    public Void visit(Times timesExpr) {
+
+        return null;
+    }
+
+    public Void visit(Division divExpr) {
+
+        return null;
+    }
+
+    public Void visit(Modulo moduloExpr) {
+
+        return null;
+    }
+
+    public Void visit(Equals equalsExpr) {
+
+        return null;
+    }
+
+    public Void visit(GreaterThan gtExpr) {
+
+        return null;
+    }
+
+    public Void visit(LessThan lessThanExpr) {
+        return null;
+    }
+
+    public Void visit(And andExpr) {
+        return null;
+    }
+
+    public Void visit(Or orExpr) {
+        return null;
+    }
+
+    public Void visit(Neg negExpr) {
+
+        return null;
+    }
+
+    public Void visit(Not notExpr) {
+        return null;
+    }
+
+    public Void visit(MethodCall methodCall) {
+
+        return null;
+    }
+
+    @Override
+    public Void visit(MethodDeclaration methodDeclaration) {
+        SymbolTable.pushFromQueue();
+        try{
+            String access,paramType= "",returnType;
+            if (methodDeclaration.getAccessModifier() == AccessModifier.ACCESS_MODIFIER_PRIVATE )
+                access = "praivate";
+            else
+                access = "public";
+            for (ParameterDeclaration arg : methodDeclaration.getArgs())
+                paramType += convertFieldType(arg.getType());
+            returnType = convertFieldType(methodDeclaration.getReturnType());
+            fw.write(".method " + access + " " + methodDeclaration.getName().getName() + "(" + paramType +")" + returnType );
+            fw.write(".limit stack 1000");
+            fw.write(".limit locals 1000");
+            for (Statement stmt : methodDeclaration.getBody())
+                stmt.accept(this);
+            fw.write(".end method");
+            SymbolTable.pop();
+        }catch (IOException e){}
+        return null;
+    }
+
+    public Void visit(Identifier identifier) {
+        return null;
+    }
+
+    public Void visit(Self self) {
+        return null;
+    }
+
+    public Void visit(IntValue intValue) {
+
+        return null;
+    }
+
+    public Void visit(NewArray newArray) {
+
+        return null;
+    }
+
+    public Void visit(BoolValue booleanValue) {
+
+        return null;
+    }
+
+    public Void visit(StringValue stringValue) {
+        return null;
+    }
+
+    public Void visit(NewClassInstance newClassInstance)
+    {
+        return null;
+    }
+
+    public Void visit(FieldCall fieldCall) {
+        return null;
+    }
+
+    public Void visit(ArrayCall arrayCall) {
+        return null;
+    }
+
+    public Void visit(NotEquals notEquals) {
+
+        return null;
+    }
+
+    public Void visit(PrintLine printStat) {
+        return null;
+    }
+
+    public Void visit(Assign assignStat) {
+
+        return null;
+    }
+
+
+
+
+    public Void visit(Return returnStat) {
+
+        return null;
+    }
+
+    public Void visit(Break breakStat) {
+        return null;
+    }
+
+    public Void visit(Continue continueStat) {
+        return null;
+    }
+
+    public Void visit(Skip skip) {
+        return null;
+    }
+
+
+    public Void visit(IncStatement incStatement) {
+        return null;
+    }
+
+    public Void visit(DecStatement decStatement) {
+        return null;
+    }
+
 
 }
